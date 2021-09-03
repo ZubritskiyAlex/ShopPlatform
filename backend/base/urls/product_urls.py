c579d7071b01e11719d4327212ade47d1ac201cb
aliaksandr.zubrytski <ZubritskiyAlexander@gmail.com>
2021-09-03 19:56:59 +0300

    add createProduct

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -1,13 +1,15 @@
 
 from django.urls import path
 
-from ..views.product_views import getProduct, getProducts, deleteProduct
+from ..views.product_views import getProduct, getProducts, deleteProduct, createProduct, updateProduct
 
 urlpatterns = [
 
     path('', getProducts, name="products"),
-    path('<str:pk>/', getProduct, name="product"),
-
+    path('create/', createProduct, name='product-create'),
+    path('update/<str:pk>/', updateProduct, name='product-update'),
     path('delete/<str:pk>/', deleteProduct, name='product-delete'),
+
+    path('<str:pk>/', getProduct, name="product"),
 
 ]
