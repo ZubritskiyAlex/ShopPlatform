4763721ff2c04caec35a90c2454e5c01af3ac5a9
aliaksandr.zubrytski <ZubritskiyAlexander@gmail.com>
2021-09-09 17:50:42 +0300

    add top products

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -1,16 +1,22 @@
 
 from django.urls import path
 
-from ..views.product_views import getProduct, getProducts, deleteProduct, createProduct, updateProduct, uploadImage
+from ..views.product_views import getProduct, getProducts, deleteProduct, createProduct, updateProduct, uploadImage, \
+    createProductReview, getTopProducts
 
 urlpatterns = [
 
     path('', getProducts, name="products"),
+
     path('create/', createProduct, name='product-create'),
     path('upload/', uploadImage, name='image-upload'),
+
+
     path('update/<str:pk>/', updateProduct, name='product-update'),
     path('delete/<str:pk>/', deleteProduct, name='product-delete'),
 
-    path('<str:pk>/', getProduct, name="product"),
+    path('top/', getTopProducts, name='top-products'),
 
+    path('<str:pk>/', getProduct, name="product"),
+    path('<str:pk>/reviews/', createProductReview, name="create-review"),
 ]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,15 +1,21 @@
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from rest_framework.response import Response
 
-from ..models import Product
+from ..models import Product, Review
 from ..serializers import ProductSerializer
 
 
 @api_view(["GET"])
 def getProducts(request):
-    products = Product.objects.all()
+    query = request.query_params.get('keyword')
+    print('query:', query)
+    if query == None:
+        query = ''
+
+    products = Product.objects.filter(name__icontains=query)
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
@@ -18,6 +24,13 @@ def getProducts(request):
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getTopProducts(request):
+    products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+    serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
@@ -78,3 +91,48 @@ def uploadImage(request):
     product.save()
 
     return Response('Image was uploaded')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    alreadyExists =product.review_set.filter(user=user).exists()
+
+    if alreadyExists:
+        content = {'details': 'Product already reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    elif data['rating'] == 0:
+        content = {'details': 'Please select a rating'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        review = Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=data['rating'],
+            comment=data['comment'],
+        )
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+
+        product.rating = total/len(reviews)
+        product.save()
+
+        return Response({'Review Added'})
+
+
+
+
+
+
+
