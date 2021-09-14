96749da1cdd0711b7a8b91d96e16fce05519d3ad
aliaksandr.zubrytski <ZubritskiyAlexander@gmail.com>
2021-09-14 13:57:25 +0300

    add docker & postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,8 +130,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proshop',
+        'USER': 'postgres',
+        'PASSWORD': '5432',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- backend/base/models.py
+++ backend/base/models.py
@@ -33,7 +33,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-# add shop fk
 
 
 class Review(models.Model):
